103a19728716b61cbf803a863946bcf24cdb2a0a
PinkyByte <csanadimisi03@gmail.com>
2023-06-15 20:31:29 +0200

    Collision works

--- individuals/Assets/Scripts/2D Movement/Movement2D.cs
+++ individuals/Assets/Scripts/2D Movement/Movement2D.cs
@@ -4,11 +4,17 @@ public class Movement2D : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D body;
+    float h;
+    float v;
 
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        body.velocity = new Vector2(h * speed, v * speed);
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        body.velocity = new Vector2(h * speed, v * speed);  
     }
 }
