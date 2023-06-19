d2386a90a20c01439d98a92b1ec896f30130bf57
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-06-19 18:25:45 -0400

    Added Different Hostility States
    ->Passive, non aggressive lacks aggression detector
    ->Hostile, Spawns Aggressive to the Player
    ->Beserk, Spawns Very Aggresive to Player, and Moves Faster

--- individuals/Assets/Scripts/2D Movement/NPCMovement.cs
+++ individuals/Assets/Scripts/2D Movement/NPCMovement.cs
@@ -6,9 +6,10 @@ public class NPCMovement : MonoBehaviour
     //External Variables
     public float speed = 3f;             
     public float walkingDistance = 10f;
-    public string aggresion = "passive";
+    public string aggresion = "readied";
     public Transform playerTransform;
     public float detectionRange = 20f;
+    
 
     //Internal Variables
     private Vector3 startPosition;
@@ -18,42 +19,75 @@ public class NPCMovement : MonoBehaviour
 
     private void Start()
     {
-        startPosition = transform.position;
-        targetPosition = startPosition + Vector3.left * walkingDistance;
+        if (aggresion == "readied" || "passive" || "hostile" || "beserk"){
+            startPosition = transform.position;
+            targetPosition = startPosition + Vector3.left * walkingDistance;
+        }
     }
 
     private void Update()
     {
         float distance = Vector3.Distance(transform.position, playerTransform.position);
-        if (aggresion == "passive"){
+        if (aggresion == "readied"){
             //This Code Handles Normal Walk Patterns
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-            if (Vector3.Distance(transform.position, targetPosition) <= 0.1f)
-            {
-                walkingForward =! walkingForward;
-                if (walkingForward){
-                    targetPosition = startPosition + Vector3.left * walkingDistance;
-                }
-                if (!walkingForward){
-                    targetPosition = startPosition;
-                }
-            }
-            //This Code Handles Enemy Detection 
-            if (distance <= detectionRange)
-            {
-                aggresion = "hostile";
-            }
+            StandardWalk();
+            aggresion = Detection(distance, detectionRange);
+        }
+        if (aggresion == "passive"){
+            //This Code is For Units that are Passive
+            StandardWalk();
         }
         if (aggresion == "hostile"){
-            //This Code Handles Chase
-            Vector3 direction = (playerTransform.position - transform.position).normalized;
-            transform.position += direction * speed * Time.deltaTime;
-            if (distance >= detectionRange)
-            {
-                aggresion = "passive";
-            }
+            //This Code When Aggresion Occurs
+            Chase(1);
+            aggresion = Detection(distance, detectionRange);
         }
+        if (aggresion == "beserk"){
+            Chase(2);
+            aggresion = Detection(distance, detectionRange * 2);
+        }
+
     }
 
-    
+    //Internal Functions
+
+    void Chase(int anger){
+        Vector3 direction = (playerTransform.position - transform.position).normalized;
+        transform.position += direction * speed * anger * Time.deltaTime;
+    }
+
+    void StandardWalk()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, targetPosition) <= 0.1f)
+        {
+            walkingForward =! walkingForward;
+            if (walkingForward){
+                targetPosition = startPosition + Vector3.left * walkingDistance;
+            }
+            if (!walkingForward){
+                targetPosition = startPosition;
+            }
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        isFlipped = !isFlipped;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    string Detection(float distance, float detectionRange)
+    {
+        if (distance >= detectionRange)
+        {
+            return "readied";
+        }
+        else 
+        {
+            return "hostile";
+        }
+    }
 }
