b41ff69643aa6d91eaec5a4fe8767f6892781323
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-07-16 11:59:54 -0400

    Added a Simple Cursor Disable Method

---
+++ individuals/Assets/Scripts/Basics/DisableCursor.cs
@@ -0,0 +1,12 @@
+void Start(){
+    Cursor.visible=false;
+    Cursor.lockState= CursorLockMode.Locked;
+}
+
+void Update(){
+    if (Input.GetKeyDown(KeyCode.Escape))
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
